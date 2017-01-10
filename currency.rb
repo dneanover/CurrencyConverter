7e8be59ab173ee0ac1d5585f25d1f8ace996464f
dneanover <damon@automatedinsights.com>
2017-01-10 16:36:58 -0500

    initialize

--- currency.rb
+++ currency.rb
@@ -1,8 +1,13 @@
 class Currency
+  attr_reader :amount, :code
 
-  def intialize(amount:, code:)
+  def initialize(amount:, code:)
     @amount = amount
     @code = code
   end
-  
+
+  def ==(other)
+    @amount == other.amount && @code == other.code
+  end
+
 end
